ce21ed19b9e573e301f567282ff84b4be160f52a
bloggermalik <timepasstime123@gmail.com>
2023-03-05 20:59:30 +0530

    ok

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -144,8 +144,12 @@ MEDIA_URL ='/images/'
 
 MEDIA_ROOT = BASE_DIR /'static'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 import os
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/") 
